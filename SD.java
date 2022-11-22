ddb79dd9b7f641121fea27eac57127424305cde2
ethanhenry3 <118320637+ethanhenry3@users.noreply.github.com>
2022-11-22 01:17:01 -0600

    Add files via upload

---
+++ SD.java
@@ -0,0 +1,15 @@
+package bored;
+
+public class SD {
+	protected String s;
+	protected int lock;
+	protected int unlock;
+	
+	
+	public SD() {
+		s = " ";
+		lock = 952381;
+		unlock = 952384;
+	}
+	
+}
