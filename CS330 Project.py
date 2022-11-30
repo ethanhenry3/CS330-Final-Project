67dd4b49dcdd752e9e8050e2b8d835a3b44ad333
ethanhenry3 <118320637+ethanhenry3@users.noreply.github.com>
2022-11-29 19:56:57 -0600

    Add files via upload

---
+++ CS330 Project.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[55]:
+
+
+import random
+class SecurityDevice:
+    def __init__(self) :
+        self.unlock = 952384
+        self.lock = 952381
+    @staticmethod
+    def main(args) :
+        a = True
+        c = 0
+        key = SecurityDevice()
+        while a :
+            try :
+                r =  random.randint(0,10000000000000)
+                if (str(key.lock).lower() in str(r).lower()):
+                    print("Locked")
+                    print(r)
+                    break
+                if (str(key.unlock).lower() in str(r).lower()):
+                    print("Unlocked")
+                    print(r)
+                    break
+                c += 1
+            except java.lang.Exception as e :
+                print("Never Found")
+            
+        print("It takes " + str(c) + " seconds to crack the pin")
+l = SecurityDevice()
+l.main("s")
+
